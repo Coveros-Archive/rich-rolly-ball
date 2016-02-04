fef24a0a5063c2df9c43674b750e7445b5cfe31d
Rich Mills <rich.mills@gmail.com>
2016-02-03 21:20:49 -0500

    initial import of rolly game that picks up objects

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		// record our offset vector at the start of the game
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame after all other activities
+	// 
+	void LateUpdate () {
+
+		// reset our position with same offset from player position.
+		transform.position = player.transform.position + offset;
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb ;
+	public float speed; 
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame. Good for input?
+	//void Update () {
+	
+	//}
+
+	// Called before physics computations
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		// If this is a pickup object, deactivate it.
+		if (other.gameObject.CompareTag ("Pick Uppable")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+}
